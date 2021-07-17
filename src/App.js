c42aac3a78fea9303628c5c1d47f17f1dae30028
David <david.nweke@outlook.com>
2021-07-17 13:46:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import './assets/Pacifico-Regular.ttf';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+function App () {
+  
+const [ loanAmount, setLoanAmount ] = useState('');
+const [ rate, setRate ] = useState( '' );
+const [ duration, setDuration ] = useState( '' );
+
+const interestRate = loanAmount * rate * duration / 100 ;
+const interest_Rate = `$${ loanAmount * rate * duration / 100 }`;
+const debt = `$${ +loanAmount + (+interestRate) }`;
+  
+return (
+    <div className="first">
+      <div className="main">
+
+        <h1 className="logo">Daveloan</h1>
+        
+        <div className="form-wrapper">
+          <div className="input">
+              <label>Loan Amount</label>
+            <input
+              type="text"
+              className="inner-text"
+              placeholder="Enter only digits (Eg. 2000.50)"
+              value={loanAmount.replace( /\D,/g, '' )}
+              onChange={e => setLoanAmount( e.target.value )}>
+              </input>
+          </div>
+          <div className="input">
+              <label>Loan Duration</label>
+              <input
+                name="duration"
+                className="inner-text"
+                placeholder="Enter the duration in years (Eg. 1year)"
+                value={duration.replace( /\D,/g, '' )}
+                onChange={e => setDuration( e.target.value )}>
+              </input>
+          </div>
+          <div className="input">
+              <label>Interest Rate</label>
+              <input
+                type="text"
+                className="inner-text"
+                placeholder="Enter the rate in % (Eg. 15%)"
+                value={rate.replace( /\D,/g, '' )}
+                onChange={e => setRate( e.target.value )}>
+              </input>
+          </div>
+        </div>
+
+        <div className="second-wrapper">
+          <div className="second-first">
+              <h1>LOAN</h1>
+              <h1>RATE</h1>
+              <h1>DEBT</h1>
+          </div>
+       
+
+        <div className="second-second">
+          <h2>{ loanAmount }</h2>
+          <h2>{ interest_Rate }</h2>
+          <h2>{ debt }</h2>
+        </div>
+        </div>
+        <footer>&copy; 2021 David Nweke. All rights reserved</footer>
+      </div>
     </div>
   );
 }
